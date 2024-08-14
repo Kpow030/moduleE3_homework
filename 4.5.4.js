042be37cc03fa0408ab17c30ea99dbb2bf2f8752
Aleksandr Titov <149224531+Kpow030@users.noreply.github.com>
2024-08-14 09:33:39 +0800

    Create 4.5.4.js

---
+++ 4.5.4.js
@@ -0,0 +1,13 @@
+class Calculator {
+  constructor() {}
+
+  power(x, n) {
+    return x ** n;
+  }
+}
+
+const calculator = new Calculator();
+\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+console.log(calculator.power(2, 3)); 
+console.log(calculator.power(5, 2)); 
+console.log(calculator.power(3, 4));
